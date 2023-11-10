8713ca456bc074d729b8ae3de7e34119dbbb1050
Florian Erbs <florian@f-erbs.de>
2023-11-11 00:34:10 +0100

    finished index html and page generation

--- http/server.go
+++ http/server.go
@@ -2,26 +2,63 @@ package http
 
 import (
 	"fmt"
+	"html/template"
+	"image/color"
 	"net/http"
+	"os"
+	"strings"
 )
 
 type Server struct {
 	Address string
 	Port    uint16
 	Uuid    string
+	Color   color.RGBA
 }
 
 func (s *Server) Start() error {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		_, err := fmt.Fprint(w, "Welcome to my website!")
-		if err != nil {
-			fmt.Println("Error writing to response writer")
-			panic(err)
-		}
-	})
+	http.HandleFunc("/", s.servePage)
 
+	replaceColorInTemplate(s)
 	var fullAddress = fmt.Sprintf("%s:%d", s.Address, s.Port)
 	fmt.Println("Server started on address: " + fullAddress)
 
 	return http.ListenAndServe(fullAddress, nil)
+}
+
+func replaceColorInTemplate(s *Server) {
+	file, err := os.Open("assets/index.html")
+	defer file.Close()
+	if err != nil {
+		return
+	}
+
+	fileInfo, _ := file.Stat()
+
+	var fileContents = make([]byte, fileInfo.Size())
+	_, err = file.Read(fileContents)
+	if err != nil {
+		return
+	}
+
+	var fileContentsString = string(fileContents)
+	var colors = fmt.Sprintf("rgba(%d, %d, %d, %d)", s.Color.R, s.Color.G, s.Color.B, s.Color.A)
+
+	fileContentsString = strings.Replace(fileContentsString, "#!#identityColor#!#", colors, 1)
+
+	err = os.WriteFile("assets/index.tmp.html", []byte(fileContentsString), 0644)
+	if err != nil {
+		return
+	}
+}
+
+func (s *Server) servePage(w http.ResponseWriter, r *http.Request) {
+	var pageData = Generate(s)
+
+	var tmpl = template.Must(template.ParseFiles("assets/index.tmp.html"))
+
+	err := tmpl.Execute(w, pageData)
+	if err != nil {
+		fmt.Println("Error executing template")
+	}
 }
